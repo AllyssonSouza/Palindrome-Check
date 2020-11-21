505438b2e5d9451b6d8b8f75d3ca2c4e63e1ec82
AllyssonSouza <allyssonsubs@icloud.com>
2020-11-22 12:57:39 +1300

    First Commit we don't forget!

---
+++ src/Palindrome.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+        System.out.println("Hi please type a word: ");
+        Scanner scan = new Scanner(System.in);
+        String original = scan.next();
+        original = original.replace(" ", "");
+
+        String reverse = "";
+        for(int i = original.length() - 1; i >= 0; i--){
+            reverse +=original.charAt(i);
+
+            /* If you want to print the reverse word
+            System.out.println(reverse); */
+        }
+
+        boolean palindrome = true;
+        for(int i =0; i < original.length(); i++){
+            if(original.charAt(i) != reverse.charAt(i)){
+                palindrome = false;
+            }
+        }
+
+        System.out.println();
+        if(palindrome){
+            System.out.println("Nice, that word is a Palindrome!");
+        }else{
+            System.out.println("That word is not a Palindrome!");
+        }
+
+
+    }
+
+}
